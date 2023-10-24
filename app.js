93a3cd52817d3a2305ee1505f8a68922f6ba33ce
Pooja Goyal <poojagoyal5106@gmail.com>
2023-10-24 14:53:20 +0530

    use cors

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 
 app.use(cors())
+app.use(cors({
+  origin:'http://127.0.0.1:5500',
+  methods:['put','get','delete','post']
+}))
+
 
 app.use('/user', userRoutes);
 
